01c5c2d462b8c97fdbe669395204c35c32748f3b
endeavourC <mcz124@wp.pl>
2022-04-30 17:45:15 +0200

    Create Preview Class, add product object to initialState with CONSTANTS, add styles to _formApp.scss

---
+++ js/Preview.js
@@ -0,0 +1,28 @@
+import { PRINT_LOCATION_BACK } from "@/js/config";
+import State from "@/js/State";
+import * as backImageUrl from "@/images/shirt_back.png";
+import * as frontImageUrl from "@/images/shirt_front.png";
+
+class Preview {
+	constructor(previewElement) {
+		this.previewElement = document.querySelector(previewElement);
+
+		this.render();
+	}
+
+	render() {
+		this.previewElement.appendChild(this.createBasicImage());
+	}
+
+	createBasicImage() {
+		const image = document.createElement("img");
+		image.src =
+			State.getProperty("product").print_location === PRINT_LOCATION_BACK
+				? backImageUrl.default
+				: frontImageUrl.default;
+
+		return image;
+	}
+}
+
+export default Preview;
--- js/config.js
+++ js/config.js
@@ -1,9 +1,13 @@
 export const API_URL = `https://picsum.photos/`;
+export const PRODUCT_BASE_PRICE = 10;
 
 export const FORM_APP_STEP_CLASS = `formApp__step`;
 export const FORM_APP_STEP_ACTIVE_CLASS = `formApp__step--active`;
 export const FORM_APP_PREV_BUTTON_ID = "prev";
 export const FORM_APP_NEXT_BUTTON_ID = "next";
+
+export const PRINT_LOCATION_BACK = "back";
+export const PRINT_LOCATION_FRONT = "front";
 
 export const initialState = {
 	currentStep: 1,
@@ -18,5 +22,9 @@ export const initialState = {
 		zip_code: "",
 		building_number: "",
 		flat_number: "",
+	},
+	product: {
+		print_location: PRINT_LOCATION_FRONT,
+		price: PRODUCT_BASE_PRICE,
 	},
 };
