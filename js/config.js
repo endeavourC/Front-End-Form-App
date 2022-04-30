fc2745d0385ec462475b8a693a4b9b1f9987c446
endeavourC <mcz124@wp.pl>
2022-04-30 13:02:19 +0200

    Create config file with Api Url and initialState

---
+++ js/config.js
@@ -0,0 +1,16 @@
+export const API_URL = `https://picsum.photos/`;
+
+export const initialState = {
+	currentStep: 1,
+	customer: {
+		name: "",
+		surname: "",
+		phone: "",
+		email: "",
+		address: "",
+		city: "",
+		zip_code: "",
+		building_number: "",
+		flat_number: "",
+	},
+};
