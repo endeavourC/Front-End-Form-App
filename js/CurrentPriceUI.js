4223f6e0fdac54e7b64c713f37a3985bfa83bbc5
endeavourC <mcz124@wp.pl>
2022-04-30 18:06:50 +0200

    Create CurrentPrice class which shows price of product

---
+++ js/CurrentPriceUI.js
@@ -0,0 +1,25 @@
+import State from "@/js/State";
+
+class CurrentPriceUI {
+	constructor(priceElement) {
+		this.priceElement = document.querySelector(priceElement);
+
+		this.render();
+	}
+
+	render() {
+		this.priceElement.innerHTML = "";
+		this.priceElement.appendChild(this.createPriceField());
+	}
+
+	createPriceField() {
+		const currentPrice = State.getProperty("product").price;
+		const priceField = document.createElement("p");
+		priceField.classList.add("formApp__price");
+		priceField.innerText = `Aktualna Cena: ${currentPrice} jednostek`;
+
+		return priceField;
+	}
+}
+
+export default CurrentPriceUI;
