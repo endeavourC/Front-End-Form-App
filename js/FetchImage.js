c646c6227855a25b76324a2fab819798b9bb1cea
endeavourC <mcz124@wp.pl>
2022-05-03 14:20:01 +0200

    Create FetchImage class which handles lorempicsum API

---
+++ js/FetchImage.js
@@ -0,0 +1,37 @@
+import { API_URL } from "@/js/config";
+import State from "@/js/State";
+
+class FetchImage {
+	static currentImageID = Math.floor(Math.random() * 100);
+
+	static imageSize = 250;
+
+	static async generateImage() {
+		const request = await fetch(
+			`${API_URL}/id/${this.currentImageID}/${this.imageSize}`
+		);
+		const getBlob = await request.blob();
+
+		State.setProperty((prevState) => ({
+			...prevState,
+			product: {
+				...prevState.product,
+				image_src: URL.createObjectURL(getBlob),
+			},
+		}));
+	}
+
+	static setNextImage() {
+		this.currentImageID++;
+		return this.generateImage();
+	}
+
+	static setPreviousImage() {
+		if (this.currentImageID === 1) return;
+
+		this.currentImageID--;
+		return this.generateImage();
+	}
+}
+
+export default FetchImage;
