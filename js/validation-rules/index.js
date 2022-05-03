bd33eafa502fcb6dd17f3465ee3b02c0fbb40fc6
endeavourC <mcz124@wp.pl>
2022-05-03 16:30:32 +0200

    Create index.js file to import every rules from one file

---
+++ js/validation-rules/index.js
@@ -0,0 +1,6 @@
+import EmailRule from "@/js/validation-rules/EmailRule";
+import PhoneRule from "@/js/validation-rules/PhoneRule";
+import RequiredRule from "@/js/validation-rules/RequiredRule";
+import ZipCodeRule from "@/js/validation-rules/ZipCodeRule";
+
+export { EmailRule, PhoneRule, RequiredRule, ZipCodeRule };
