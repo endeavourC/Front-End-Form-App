aaef8975107819415135389f4980d9c2b7a19b16
endeavourC <mcz124@wp.pl>
2022-05-03 14:21:50 +0200

    Create Pick Image View

---
+++ js/steps/PickImageStep.js
@@ -0,0 +1,54 @@
+import Step from "@/js/Step";
+import FetchImage from "@/js/FetchImage";
+import State from "@/js/State";
+
+const pickImageView = (rootElement) => {
+	rootElement.innerHTML = "";
+
+	const handleNextImage = (ev) => {
+		ev.preventDefault();
+		FetchImage.setNextImage();
+	};
+
+	const handlePrevImage = (ev) => {
+		ev.preventDefault();
+		FetchImage.setPreviousImage();
+	};
+
+	const headline = document.createElement("h2");
+	headline.innerText = "Wybierz zdjęcie";
+	headline.classList.add("headline", "headline--small");
+
+	const currentImage = document.createElement("img");
+	currentImage.src = State.getProperty("product").image_src;
+	currentImage.width = "250";
+	currentImage.height = "250";
+
+	const buttonsContainer = document.createElement("div");
+	buttonsContainer.classList.add("buttonsContainer");
+
+	const previousButton = document.createElement("button");
+	previousButton.innerText = "Poprzednie zdjęcie";
+	previousButton.classList.add("button");
+	previousButton.addEventListener("click", handlePrevImage);
+
+	const nextButton = document.createElement("button");
+	nextButton.innerText = "Następne zdjęcie";
+	nextButton.classList.add("button");
+	nextButton.addEventListener("click", handleNextImage);
+
+	buttonsContainer.appendChild(previousButton);
+	buttonsContainer.appendChild(nextButton);
+
+	rootElement.appendChild(headline);
+	rootElement.appendChild(currentImage);
+	rootElement.appendChild(buttonsContainer);
+};
+
+const pickImageStep = new Step(
+	"Wybierz grafikę",
+	"choose-graphic",
+	pickImageView
+);
+
+export default pickImageStep;
