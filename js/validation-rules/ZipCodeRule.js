72c52fa365bb1d26dbe0f5d49cbd895e9fbdf720
endeavourC <mcz124@wp.pl>
2022-05-03 16:37:12 +0200

    Fix ZipCodeRule

--- js/validation-rules/ZipCodeRule.js
+++ js/validation-rules/ZipCodeRule.js
@@ -4,9 +4,7 @@ class ZipCodeRule extends Rule {
 	static message = "Niepoprawny kod pocztowy";
 
 	static isInvalid(zipcode) {
-		const re =
-			/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-		return !re.test(zipcode);
+		return !zipcode.match(/^[0-9]{2}-[0-9]{3}$/);
 	}
 }
 
