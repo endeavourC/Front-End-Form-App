36dab4d2a1b15b9790c4c34d7736dc30e7f0dce4
endeavourC <mcz124@wp.pl>
2022-05-04 16:16:23 +0200

    Implement all client logic into start method in App.js class

--- js/App.js
+++ js/App.js
@@ -1,11 +1,42 @@
 import State from "@/js/State";
 
 class App {
+	constructor(stepsUI, previewUI, currentPriceUI) {
+		this.stepsUI = stepsUI;
+		this.previewUI = previewUI;
+		this.currentPriceUI = currentPriceUI;
+	}
+
 	addStep(step) {
 		State.setProperty((prevState) => ({
 			...prevState,
 			steps: [...prevState.steps, step],
 		}));
+	}
+
+	start() {
+		this.stepsUI.render();
+
+		State.subscribe((state, oldState) => {
+			if (state.currentStep !== oldState.currentStep) {
+				this.stepsUI.handleDisableButtons();
+				this.stepsUI.render(true);
+				this.currentPriceUI.render();
+			}
+		});
+
+		State.subscribe((state, oldState) => {
+			if (state.disableNextButton !== oldState.disableNextButton) {
+				this.stepsUI.renderHeadingElement();
+			}
+		});
+
+		State.subscribe((state, oldState) => {
+			if (state.product !== oldState.product) {
+				this.previewUI.render();
+				this.stepsUI.render();
+			}
+		});
 	}
 }
 
